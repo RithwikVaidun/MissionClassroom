62e495d5bd307ffe2fd94fdd88d6fc25832b3c5a
rvaidun <{ID}+{username}@users.noreply.github.com>
2021-08-02 23:26:27 -0700

    created fields for all classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import firebase from "firebase";
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
 
-interface cls {
+interface Cls {
   name: string;
   teacher: string;
   period: number;
 }
 function App() {
   const firebaseApp = firebase.apps[0];
-  const [name, setName] = useState<string>()
+  const [cls, setCls] = useState<Cls[]>([{ name: "", teacher: "", period: 0 }]);
+  function writetoFirebase(e: any) {
+    e.preventDefault();
+    firebaseApp.firestore().collection("test").doc("test").set(cls);
+  }
+  const items = [];
+  for (var x = 0; x < 6; x++) {
+    let i = x;
+    items.push(
+      <>
+        <TextField
+          placeholder="Class Name"
+          onChange={(e) => {
+            let newArr = [...cls];
+            newArr[i] = { ...newArr[i], name: e.target.value };
+            setCls(newArr);
+          }}
+        />
+        <TextField
+          placeholder="Teacher"
+          onChange={(e) => {
+            let newArr = [...cls];
+            newArr[i] = { ...newArr[i], teacher: e.target.value };
+            setCls(newArr);
+          }}
+        />
+        <TextField
+          placeholder="Period"
+          onChange={(e) => {
+            let newArr = [...cls];
+            newArr[i] = { ...newArr[i], period: parseInt(e.target.value) };
+            setCls(newArr);
+          }}
+        />
+      </>
+    );
+  }
   return (
     <div>
       <h1>React & Firebase</h1>
       <h2>By @farazamiruddin</h2>
-      <TextField onChange={(e) => {
-        setName(e.target.value)
-      }}/>
-      <p>{name}</p>
+      {items}
+      <Button onClick={writetoFirebase}> Hi </Button>
+
+      <p>{JSON.stringify(cls)}</p>
     </div>
   );
 }
