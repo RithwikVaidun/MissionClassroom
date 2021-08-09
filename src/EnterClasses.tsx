af657ab543a013a52732862256463de35c298bff
rvaidun <{ID}+{username}@users.noreply.github.com>
2021-08-09 14:15:15 -0700

    pushed

--- src/EnterClasses.tsx
+++ src/EnterClasses.tsx
@@ -85,17 +85,14 @@ function EnterClasses(props: EnterClassesInterface) {
                           period: e.target.value as number,
                         };
                         setCls(newArr);
-                        // if (e.target.value) {
-                        // setCls([...cls, { period: e.target.value }]);
-                        // }
                       }}
                     >
-                      {/* <MenuItem value={1}>1</MenuItem>
+                      <MenuItem value={1}>1</MenuItem>
                       <MenuItem value={2}>2</MenuItem>
                       <MenuItem value={3}>3</MenuItem>
                       <MenuItem value={4}>4</MenuItem>
                       <MenuItem value={5}>5</MenuItem>
-                      <MenuItem value={6}>6</MenuItem> */}
+                      <MenuItem value={6}>6</MenuItem>
                     </Select>
                   </div>
                   <div style={{ float: "left" }}>
